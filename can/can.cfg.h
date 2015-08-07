9c8ecbdafcb2801fb3b22d363ae097dc7f1ceaf2
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-08-07 15:27:05 +0200

    added license information to source code

--- can/can.cfg.h
+++ can/can.cfg.h
@@ -6,6 +6,19 @@
 // Version		: 0.1
 // Target MCU	: Fujitsu MB96300 series
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 ///	\ingroup can_lowlevel
 ///	\defgroup can_lowlevel_configuration Internal configurations (can.cfg.h)
 ///	\par Overview
